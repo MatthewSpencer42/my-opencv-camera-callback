4649df16e2d19228225768b02af585a4ef229167
MatthewSpencer42 <matthewspencer42@gmail.com>
2023-09-28 18:31:22 +0100

    add

--- testcallback.cpp
+++ testcallback.cpp
@@ -16,28 +16,26 @@
 
 struct CameraCallback:Camera::SceneCallback{
     void nextScene(const cv::Mat&mat){ //takes frame, does function
-        //bool x = false; //only if using if condition
+        
         std::vector<cv::Rect> xs; //declare a rectangular vector to store all found hits
     	cv::Mat gray; //declare matrix for grayscale image of frame
     	cv::Mat sized; //declare matrix for resized image of frame
     	cv::CascadeClassifier cascade; //declare cascade as a cascade classifier in preparation
     	cascade.load("cascade.xml");//input cascade file
-    	resize(mat, sized,cv::Size(600,300),0,0,cv::INTER_LINEAR);//resize if needed
-    		cv::cvtColor(sized,gray,CV_BGR2GRAY); //change to grayscale
-    		xs.clear();
-    		cascade.detectMultiScale(gray, xs, 1.1, 0, 0, cv::Size(50, 50), cv::Size(100,100));//use detectMultiScale and get rectangles
+    	resize(mat, sized,cv::Size(600,300),0,0,cv::INTER_LINEAR);//resized to values that showed best results with detection parameters
+    	cv::cvtColor(sized,gray,CV_BGR2GRAY); //change to grayscale
+    	xs.clear(); //clear the rectangles each loop
+    	cascade.detectMultiScale(gray, xs, 1.1, 0, 0, cv::Size(50, 50), cv::Size(100,100));//use detectMultiScale and get rectangles
     
-    		//if (xs.empty()){//maybe add if to only do following if rectangles exist
-    	
-    		for( std::vector<cv::Rect>::const_iterator r = xs.begin(); r != xs.end(); r++){
-            		if((r->x + ((r->width)/2)) > 300){
-            			std::cout << "The x is to the right. At " << (r->x + ((r->width)/2)) << "," << (r->y + ((r->height)/2)) << ". \n";
-            			}
-            		else if((r->x + ((r->width)/2)) < 300){
-            			std::cout << "The x is to the left. At " << (r->x + ((r->width)/2)) << "," << (r->y + ((r->height)/2)) << ". \n";
-            			}
+    	for( std::vector<cv::Rect>::const_iterator r = xs.begin(); r != xs.end(); r++){ //loops for every rectangle with r given xs values
+            	if((r->x + ((r->width)/2)) > 300){ //x coordinate for middle of rectangle further right than the middle
+            		std::cout << "The x is to the right. At " << (r->x + ((r->width)/2)) << "," << (r->y + ((r->height)/2)) << ". \n";
+            		}
+            	else if((r->x + ((r->width)/2)) < 300){ //x coordinate for middle of rectangle further left than middle
+            		std::cout << "The x is to the left. At " << (r->x + ((r->width)/2)) << "," << (r->y + ((r->height)/2)) << ". \n";
+            		}
             		//maybe have value indicating if left or right, then after the for loop, decide on direction to move
-        	}//add rectangles to original image and notifies direction
+        }
         	
     }
 };
